b8f74c45f2629d38d00325008b3420b7641bd591
BateCheaterDemon <596891579@qq.com>
2025-02-22 16:45:43 +0800

    [PythonLearning 2.22 Basic]

--- PythonLearning/Sec1_Basic/first_step.py
+++ PythonLearning/Sec1_Basic/first_step.py
@@ -1,5 +1,11 @@
-print("hello world")
-
-name = input("please input your name:")
-print("hello",name)
-
+# 如果使用match语句，就可以写得更加简洁(match 是python3.10以上的功能)：
+score = 'B'
+match score:
+    case 'A':
+        print('score is A')
+    case 'B':
+        print('score is B')
+    case 'C':
+        print('score is C')
+    case _:
+        print('score is invalid')
